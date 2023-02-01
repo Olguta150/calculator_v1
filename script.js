a34f2e2e7417af7a2d6a990ff622da087a6159e6
Olguta Fratila <fratilaolguta150@gmail.com>
2023-02-01 13:03:27 +0200

    Add substract, add, multiply, divide and reminder functions to make operations. Add operate function to select the correct operation.

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function reminder(a, b) {
+    return a % b;
+}
+
+function operate(operator, a, b) {
+    if(operator === add) {
+        return add(a, b);
+    } else if(operator === substract) {
+        return substract(a, b);
+    } else if(operator === multiply) {
+        return multiply(a, b);
+    } else if(operator === divide) {
+        return divide(a, b);
+    } else {
+        return reminder(a, b);
+    }
+}
+
+operate(reminder, 63, 4);
+
 function onClickStyle() {
     let digits = document.querySelectorAll('.digit');
 
